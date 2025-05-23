f658773cb83dcf3de17378f57df40e4764ba551e
Sayushman <ayushman.sengupta@s.amity.edu>
2025-05-23 16:06:59 +0530

    pickle update

--- web.py
+++ web.py
@@ -16,10 +16,12 @@ def recommend(movie):
     return recommend_movies
 
 
-movies_dict = pickle.load(open('/home/aayu/Documents/PROJECTS/Movie-Reccomandation-System/movies_dict.pkl','rb'))
+# movies_dict = pickle.load(open('/home/aayu/Documents/PROJECTS/Movie-Reccomandation-System/movies_dict.pkl','rb'))
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('/home/aayu/Documents/PROJECTS/Movie-Reccomandation-System/similarity.pkl','rb'))
+# similarity = pickle.load(open('/home/aayu/Documents/PROJECTS/Movie-Reccomandation-System/similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
 
 st.title('Movie Recommender System')
 
